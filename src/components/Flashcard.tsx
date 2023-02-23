9ddabfa526a100c951e1d8df3878f5a5d872f591
Jiali Zhang <zhangjiali86@gmail.com>
2023-02-23 21:26:54 +0100

    clean code

--- src/components/Flashcard.tsx
+++ src/components/Flashcard.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { IFlashcard } from '../Interface';
 import answer from '../pic/answer.jpg';
-import  question from '../pic/question.jpeg'
+import question from '../pic/question.jpeg'
 
 
 type Props = {
@@ -12,31 +12,32 @@ function Flashcard({ flashcard }: Props) {
   const [flip, setFlip] = useState<boolean>(false);
 
   return (
-    <div className="card w-96 bg-base-100 shadow-xl image-full"
+    <div className="card w-96 bg-base-100 shadow-xl image-full h-[300px] h-[300px] "
       onClick={() => setFlip(!flip)}>
+
       {flip ?
-        <figure><img src={answer} alt="Answer" /></figure> 
+        <>
+          <figure><img src={answer} alt="Answer" className="w-[300px]"/></figure>
+          <div className="card-body">
+            <h2 className="card-title">Answer:</h2>
+            <p>{flashcard.answer}</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-outline btn-warning">Show Question</button>
+            </div>
+          </div>
+        </>
         :
-        <figure><img src={question} alt="Question" /></figure>
+        <>
+          <figure><img src={question} alt="Question" className="w-[300px]"/></figure>
+          <div className="card-body">
+            <h2 className="card-title">Question:</h2>
+            <p>{flashcard.question}</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-outline btn-warning">Show Answer</button>
+            </div>
+          </div>
+        </>
       }
-
-      <div className="card-body">
-
-        {flip ?
-          <h2 className="card-title">Answer:</h2> 
-          :
-          <h2 className="card-title">Question:</h2>}
-
-        <p>{flip ? flashcard.answer : flashcard.question}</p>
-
-        <div className="card-actions justify-end">
-          {flip ?
-            <button className="btn btn-outline btn-warning">Show Question</button>
-            :
-            <button className="btn btn-outline btn-warning">Show Answer</button>
-          }
-        </div>
-      </div>
 
     </div>
   )
